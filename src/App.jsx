d7fece859b668b550805e24b9a1c527125dd3b6a
T.D Rasindu Dulshan Siriwardhana <85836178+DulshanSiriwardhana@users.noreply.github.com>
2024-08-29 23:59:36 +0530

    navbar created

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="w-full h-full">
+    <div className="w-full h-full font-LexendDecaRegular">
       <Home />
     </div>
   );
