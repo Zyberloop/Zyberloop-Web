0e58044876ddb0c7a7b678434f90291de2f52359
sasandamanahara <sasandamanahara12@gmail.com>
2024-08-30 21:26:15 +0530

    Update

---
+++ src/sections/HeroSection.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function HeroSection() {
+    return (
+        <div className='flex flex-col justify-center items-center mt-16 space-y-5'>
+            <div className='text-white text-7xl'>Secure a bright future for your</div>
+            <div className='text-Card_Button text-7xl'>Business</div>
+            <div className="h-[0.7px] w-2/5 bg-gradient-to-r from-primary via-Card_Button to-primary"></div>
+            <div className='text-white text-2xl  text-center'>Experience exponential growth with Zyberloop's innovative <br /> web development solutions.</div>
+        </div >
+
+    )
+}
