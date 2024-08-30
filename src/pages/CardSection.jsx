a50ec7e22fbbfe03a48e0eaf9cd5c4961126c49a
sasandamanahara <sasandamanahara12@gmail.com>
2024-08-30 22:25:06 +0530

    responsive

--- src/pages/CardSection.jsx
+++ src/pages/CardSection.jsx
@@ -4,44 +4,47 @@ import { Layers2 } from 'lucide-react';
 
 const CardSection = () => {
     return (
-        <div className='flex flex-col gap-10 py-10'>
-            <div className="flex justify-center items-center gap-10 ">
+        <div className='flex items-center justify-center '>
+            <div className='grid xl:grid-cols-3  md:grid-cols-2 grid-cols-1 gap-10 py-10 px-10 max-w-[1600px]'>
+
                 <Card
-                    icon={<Layers2 size={90} />}
+                    icon={<Layers2 className='h-auto xl:w-20  md:w-14 w-10' />}
                     title="Create"
                     description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
                     buttonText="Get Started" />
                 <Card
-                    icon={<Layers2 size={90} />}
+                    icon={<Layers2 className='h-auto xl:w-20  md:w-14 w-10' />}
                     title="Create"
                     description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
                     buttonText="Get Started" />
                 <Card
-                    icon={<Layers2 size={90} />}
+                    icon={<Layers2 className='h-auto xl:w-20  md:w-14 w-10' />}
                     title="Create"
                     description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
                     buttonText="Get Started" />
+
+
+
+
+                <Card
+                    icon={<Layers2 className='h-auto xl:w-20  md:w-14 w-10' />}
+                    title="Create"
+                    description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+                    buttonText="Get Started" />
+                <Card
+                    icon={<Layers2 className='h-auto xl:w-20  md:w-14 w-10' />}
+                    title="Create"
+                    description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+                    buttonText="Get Started" />
+                <Card
+                    icon={<Layers2 className='h-auto xl:w-20  md:w-14 w-10' />}
+                    title="Create"
+                    description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+                    buttonText="Get Started" />
+
 
             </div>
 
-            <div className="flex justify-center items-center gap-10 ">
-                <Card
-                    icon={<Layers2 size={90} />}
-                    title="Create"
-                    description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-                    buttonText="Get Started" />
-                <Card
-                    icon={<Layers2 size={90} />}
-                    title="Create"
-                    description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-                    buttonText="Get Started" />
-                <Card
-                    icon={<Layers2 size={90} />}
-                    title="Create"
-                    description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-                    buttonText="Get Started" />
-
-            </div>
         </div>
 
 
