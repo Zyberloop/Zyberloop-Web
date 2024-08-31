5076c8fd5075fde6ea9d6dbc66c527afb5d4c117
sasandamanahara <sasandamanahara12@gmail.com>
2024-08-31 19:00:36 +0530

    Update

--- src/sections/ProjectSection.jsx
+++ src/sections/ProjectSection.jsx
@@ -3,13 +3,11 @@ import ProjectCard from "../components/PrjectCard";
 const ProjectSection = () => {
   return (
     <div className="w-full h-full ">
-      <div className="w-full max-w-max mx-auto h-full space-y-4">
-        <div className="gradient-text text-4xl sm:text-5xl md:text-6xl lg:text-7xl text-center">
-          Innovative Projects That Shine
+      <div className="w-full max-w-max mx-auto h-full space-y-4 flex flex-col justify-center items-center">
+        <div className="gradient-text text-4xl sm:text-5xl md:text-6xl lg:text-7xl text-center max-w-7xl">
+          Innovative Projects That Shine Our Effort
         </div>
-        <div className="gradient-text2 bg-clip-text text-transparent text-4xl sm:text-5xl md:text-6xl lg:text-7xl text-center ">
-          Our Effort
-        </div>
+
         <div className="max-w-[1160px] w-full mx-auto">
           <ProjectCard
             tag={"Featured  Project"}
