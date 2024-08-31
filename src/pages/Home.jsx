f6e7f1068ed74e05895ff68d0f9a403beba918b2
sasandamanahara <sasandamanahara12@gmail.com>
2024-08-31 08:18:51 +0530

    Footer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import HeroSection from "../sections/HeroSection";
 import TechStack from "../sections/TechStack";
@@ -10,6 +11,7 @@ const Home = () => {
       <HeroSection />
       <CardSection />
       <TechStack />
+      <Footer />
     </div>
   );
 };
