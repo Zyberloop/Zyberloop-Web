48f35b363668923c8e74ee1a5f92a7568a850659
Sanduni Manjula <sandunimanju.27@gmail.com>
2024-08-31 22:18:00 +0530

    icon

--- src/sections/CardSection.jsx
+++ src/sections/CardSection.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import Card from '../components/Card';
-import { Layers2 } from 'lucide-react';
+import { PanelsTopLeft } from 'lucide-react';
+import { TabletSmartphone } from 'lucide-react';
+import { ChartNetwork } from 'lucide-react';
+import { ShieldHalf } from 'lucide-react';
+import { CloudCog } from 'lucide-react';
+import { ChevronsLeftRightEllipsis } from 'lucide-react';
 
 const CardSection = () => {
     return (
@@ -8,17 +13,17 @@ const CardSection = () => {
             <div className='grid xl:grid-cols-3  md:grid-cols-2 grid-cols-1 gap-10 py-10 px-10 max-w-[1600px]'>
 
                 <Card
-                    icon={<Layers2 className='h-auto xl:w-20  md:w-14 w-10' />}
+                    icon={<PanelsTopLeft className='h-auto xl:w-20  md:w-14 w-10' />}
                     title="Web Development"
                     description="Zyberloop creates robust, user-friendly websites to enhance businesses' online presence, tailored for local markets."
                     buttonText="Get Started" />
                 <Card
-                    icon={<Layers2 className='h-auto xl:w-20  md:w-14 w-10' />}
+                    icon={<TabletSmartphone className='h-auto xl:w-20  md:w-14 w-10' />}
                     title="Mobile App Development"
                     description="We develop mobile apps that enhance customer engagement, focusing on usability, functionality, and performance."
                     buttonText="Get Started" />
                 <Card
-                    icon={<Layers2 className='h-auto xl:w-20  md:w-14 w-10' />}
+                    icon={<ChartNetwork className='h-auto xl:w-20  md:w-14 w-10' />}
                     title="ERP Solutions"
                     description="We provide customized ERP solutions to streamline operations and improve business efficiency."
                     buttonText="Get Started" />
@@ -27,17 +32,17 @@ const CardSection = () => {
 
 
                 <Card
-                    icon={<Layers2 className='h-auto xl:w-20  md:w-14 w-10' />}
+                    icon={<ShieldHalf className='h-auto xl:w-20  md:w-14 w-10' />}
                     title="Logos & Brand Designing"
                     description="Zyberloop creates impactful logos and brand designs to establish a strong, memorable identity for businesses."
                     buttonText="Get Started" />
                 <Card
-                    icon={<Layers2 className='h-auto xl:w-20  md:w-14 w-10' />}
+                    icon={<CloudCog className='h-auto xl:w-20  md:w-14 w-10' />}
                     title="Cloud Solutions"
                     description="We offer scalable cloud solutions to enhance data security, accessibility, and business agility."
                     buttonText="Get Started" />
                 <Card
-                    icon={<Layers2 className='h-auto xl:w-20  md:w-14 w-10' />}
+                    icon={<ChevronsLeftRightEllipsis className='h-auto xl:w-20  md:w-14 w-10' />}
                     title="Social Media Management"
                     description="We manage social media platforms to boost brand visibility, engage audiences, and drive business growth."
                     buttonText="Get Started" />
