2e876dbdf00303d57bb3a2599112181c00eecf45
sasandamanahara <sasandamanahara12@gmail.com>
2024-09-02 21:22:46 +0530

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Career from "./pages/Career";
-
+import ErpProject from "./pages/ErpProject.jsx";
+import "./App.css";
 function App() {
   return (
     <Router>
@@ -9,10 +10,10 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/careers" element={<Career />} />
+          <Route path="/erp-project" element={<ErpProject />} />
         </Routes>
       </div>
     </Router>
   );
 }
-
 export default App;
