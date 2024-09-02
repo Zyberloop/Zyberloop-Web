45a096ee7401247260078eaf5aebbea48da90e58
T.D Rasindu Dulshan Siriwardhana <85836178+DulshanSiriwardhana@users.noreply.github.com>
2024-09-02 22:53:27 +0530

    career page modified

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -17,7 +17,7 @@ const Popup = ({ isOpen, onClose, jobType }) => {
 
   return (
     <div className="fixed inset-0 bg-gray-800 bg-opacity-70 flex items-center justify-center z-50">
-      <div className="bg-[#FEFBF6] p-6 rounded-lg shadow-lg w-full max-w-md relative">
+      <div className="bg-white p-6 rounded-lg shadow-lg w-full max-w-md relative">
         <h2 className="text-2xl font-bold mb-4 text-[#3D3C42]">
           Apply for {jobType}
         </h2>
