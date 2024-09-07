e5705cf13c7f0e20188836deb68ff99e8b52a132
Hashith Sithuruwan <mailtohashith@gmail.com>
2024-09-07 23:47:09 +0530

    fixed a bug on main.jsx file

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,4 @@ import App from "./App.jsx";
 import "./index.css";
 import "./assets/fonts/fonts.css";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+createRoot(document.getElementById("root")).render(<App />);
